8e3e5d1f9772b5c2b4581014415809c2f488386c
magicmn <julianklein1@gmail.com>
2018-03-12 15:54:10 +0100

    s

--- PIEnTra/src/model/M_Kunde.java
+++ PIEnTra/src/model/M_Kunde.java
@@ -8,4 +8,41 @@ public class M_Kunde {
 	private String email;
 	private String telefon;
 	private String ansprechpartner;
+	
+	public int getKundenID() {
+		return kundenID;
+	}
+	public void setKundenID(int kundenID) {
+		this.kundenID = kundenID;
+	}
+	public String getFirmenname() {
+		return firmenname;
+	}
+	public void setFirmenname(String firmenname) {
+		this.firmenname = firmenname;
+	}
+	public String getBranche() {
+		return branche;
+	}
+	public void setBranche(String branche) {
+		this.branche = branche;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+	public String getAnsprechpartner() {
+		return ansprechpartner;
+	}
+	public void setAnsprechpartner(String ansprechpartner) {
+		this.ansprechpartner = ansprechpartner;
+	}
 }
