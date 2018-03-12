895a59d1e0c566db9b757a5221678c60ff81ed13
Teilnehmer <Teilnehmer@CSC-Akademie_02>
2018-03-12 16:21:25 +0100

    angelegt

---
+++ PIEnTra/src/model/M_Produkt.java
@@ -0,0 +1,37 @@
+package model;
+
+public class M_Produkt {
+
+	private int produktID;
+	private String bezeichnung;
+	private String beschreibung;
+	private Double version;
+	
+	public int getProduktID() {
+		return produktID;
+	}
+	public void setProduktID(int produktID) {
+		this.produktID = produktID;
+	}
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+	public String getBeschreibung() {
+		return beschreibung;
+	}
+	public void setBeschreibung(String beschreibung) {
+		this.beschreibung = beschreibung;
+	}
+	public Double getVersion() {
+		return version;
+	}
+	public void setVersion(Double version) {
+		this.version = version;
+	}
+	
+	
+	
+}
