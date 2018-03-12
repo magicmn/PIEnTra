498f89b06b6c51819f010442cb07428d86b1d845
magicmn <julianklein1@gmail.com>
2018-03-12 15:49:03 +0100

    Model exists

---
+++ PIEnTra/src/model/M_ManagmentAssistent.java
@@ -0,0 +1,28 @@
+package model;
+
+public class M_ManagmentAssistent {
+	private int mitarbeiterID;
+	private String name;
+	private String vorname;
+	
+	
+	
+	public int getMitarbeiterID() {
+		return mitarbeiterID;
+	}
+	public void setMitarbeiterID(int mitarbeiterID) {
+		this.mitarbeiterID = mitarbeiterID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+}
