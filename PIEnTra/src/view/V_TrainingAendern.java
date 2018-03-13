9563a7544149bee20d65458f2cb0852ad5eaf1b7
Teilnehmer <Teilnehmer@CSC-Akademie_02>
2018-03-13 13:09:33 +0100

    verbessert

--- PIEnTra/src/view/V_TrainingAendern.java
+++ PIEnTra/src/view/V_TrainingAendern.java
@@ -12,32 +12,31 @@ public class V_TrainingAendern extends JFrame {
 		V_TrainingAendern trainingAendern = new V_TrainingAendern();
 	}
 	
-	
 	//JLabels und JTextfields zur Eingabe
-	JTextField txt_trainingsid = new JTextField();  						//Text-Feld Trainings-ID
-	JLabel lbl_trainingsid = new JLabel("Trainings-ID:");					//Label Trainings-ID
-	JTextField txt_firmenname = new JTextField();  							//Text-Feld Firmenname
-	JLabel lbl_firmenname = new JLabel("Firmenname:");						//Label Firmenname
-	JTextField txt_ansprechpartner = new JTextField(); 						//Text-Feld Ansprechpartner
-	JLabel lbl_ansprechpartner = new JLabel("Ansprechpartner:");			//Label Ansprechpartner
-	JTextField txt_produktbeschreibung = new JPasswordField();  			//Text-Feld Produktbeschreibung
-	JLabel lbl_produktbeschreibung = new JLabel("Produktbeschreibung:");	//Label Produktbeschreibung
-	JTextField txt_startdatum = new JTextField();							//Text-Feld Startdatum
-	JLabel lbl_startdatum = new JLabel("Startdatum:");						//Label Startdatum
-	JTextField txt_enddatum = new JTextField();								//Text-Feld Enddatum
-	JLabel lbl_enddatum = new JLabel("Enddatum:");							//Label Enddatum
-	JTextField txt_tage = new JTextField();									//Text-Feld Tage
-	JLabel lbl_tage = new JLabel("Tage:");									//Label Tage
-	JTextField txt_trainer = new JTextField();								//Text-Feld Trainer
-	JLabel lbl_trainer = new JLabel("Trainer:");							//Label Trainer
-	JTextField txt_ort = new JTextField();									//Text-Feld Ort
-	JLabel lbl_ort = new JLabel("Ort:");									//Label Ort
-	JTextField txt_bemerkungen = new JTextField();							//Text-Feld Bemerkungen
-	JLabel lbl_bemerkungen = new JLabel("Bemerkungen:");					//Label Bemerkungen
+	JTextField txt_trainingsid = new JTextField();  							//Text-Feld Trainings-ID
+	JLabel lbl_trainingsid = new JLabel("Trainings-ID:");						//Label Trainings-ID
+	JTextField txt_firmenname = new JTextField();  								//Text-Feld Firmenname
+	JLabel lbl_firmenname = new JLabel("Firmenname:");							//Label Firmenname
+	JTextField txt_ansprechpartner = new JTextField(); 							//Text-Feld Ansprechpartner
+	JLabel lbl_ansprechpartner = new JLabel("Ansprechpartner:");				//Label Ansprechpartner
+	JTextField txt_produktbeschreibung = new JPasswordField();  				//Text-Feld Produktbeschreibung
+	JLabel lbl_produktbeschreibung = new JLabel("Produktbeschreibung:");		//Label Produktbeschreibung
+	JTextField txt_startdatum = new JTextField();								//Text-Feld Startdatum
+	JLabel lbl_startdatum = new JLabel("Startdatum:");							//Label Startdatum
+	JTextField txt_enddatum = new JTextField();									//Text-Feld Enddatum
+	JLabel lbl_enddatum = new JLabel("Enddatum:");								//Label Enddatum
+	JTextField txt_tage = new JTextField();										//Text-Feld Tage
+	JLabel lbl_tage = new JLabel("Tage:");										//Label Tage
+	JTextField txt_trainer = new JTextField();									//Text-Feld Trainer
+	JLabel lbl_trainer = new JLabel("Trainer:");								//Label Trainer
+	JTextField txt_ort = new JTextField();										//Text-Feld Ort
+	JLabel lbl_ort = new JLabel("Ort:");										//Label Ort
+	JTextField txt_bemerkungen = new JTextField();								//Text-Feld Bemerkungen
+	JLabel lbl_bemerkungen = new JLabel("Bemerkungen:");						//Label Bemerkungen
 
 	//JButtons erstellen
-	JButton btn_suchen = new JButton("Training suchen");					//Button suchen
-	JButton btn_ressourcenAendern = new JButton("Ressourcen ändern");		//Button Ressourcen ändern
+	JButton btn_suchen = new JButton("Training suchen");						//Button suchen
+	JButton btn_ressourcenAendern = new JButton("Ressourcen ändern");			//Button Ressourcen ändern
 	JButton btn_TrainingAktualisieren = new JButton("Training aktualisieren");	//Button Training aktualisieren
 	JButton btn_zurueck = new JButton("Zurück zum Hauptmenü");					//Button Zurück
 
@@ -64,34 +63,34 @@ public class V_TrainingAendern extends JFrame {
 	//Dem mainPanel und buttonPanel hinzufügen + nächste Zeile mit new Dimension
 	mainPanel.add(lbl_trainingsid);
 	mainPanel.add(txt_trainingsid);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(50,50)));
 	mainPanel.add(lbl_firmenname);
 	mainPanel.add(txt_firmenname);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_ansprechpartner);
 	mainPanel.add(txt_ansprechpartner);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_produktbeschreibung);
 	mainPanel.add(txt_produktbeschreibung);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_startdatum);
 	mainPanel.add(txt_startdatum);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_enddatum);
 	mainPanel.add(txt_enddatum);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_tage);
 	mainPanel.add(txt_tage);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_trainer);
 	mainPanel.add(txt_trainer);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_ort);
 	mainPanel.add(txt_ort);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	mainPanel.add(lbl_bemerkungen);
 	mainPanel.add(txt_bemerkungen);
-	mainPanel.add(Box.createRigidArea(new Dimension(0,5)));
+	mainPanel.add(Box.createRigidArea(new Dimension(0,0)));
 	
 	panel_south.add(btn_suchen);
 	panel_south.add(Box.createRigidArea(new Dimension(5,0)));
@@ -102,11 +101,9 @@ public class V_TrainingAendern extends JFrame {
 	panel_south.add(btn_zurueck);
 	panel_south.add(Box.createRigidArea(new Dimension(5,0)));
 
-
 	//Hinzufügen der JPanels zum BorderLayout
 	getContentPane().add(mainPanel);
 	getContentPane().add(panel_south, BorderLayout.SOUTH);
-
 	
 	setVisible(true);
 }
