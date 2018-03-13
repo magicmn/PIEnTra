5a64981ba986117c55240347f6a05d40ae6360c3
Konstantin <Konstantin@DESKTOP-FURLC90>
2018-03-13 15:43:31 +0100

    hallö

---
+++ PIEnTra/src/model/M_Trainer.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.ArrayList;
+
+public class M_Trainer extends M_Person {
+	private static int interneID;
+	private int trainerID; 
+	private ArrayList<M_Produkt> produkte;
+	private ArrayList<M_Training> trainings ; //muss durch die Methode trainingHinzufügen() initialisiert werden
+	public M_Trainer(String vorname, String nachname, M_Produkt ... produkte) {
+		super(vorname, nachname);
+		setInterneID(getInterneID()+1);
+		setTrainerID(getInterneID());
+		this.produkte = new ArrayList<M_Produkt>();
+		for(int i  = 0; i<produkte.length; i++) {
+			this.produkte.add(produkte[i]);
+			
+			}
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public ArrayList<M_Training> getTrainings() {
+		return trainings;
+	}
+
+
+
+	public void setTrainings(ArrayList<M_Training> trainings) {
+		this.trainings = trainings;
+	}
+
+
+
+	@Override
+	public String getVorname() {
+		// TODO Auto-generated method stub
+		return super.getVorname();
+	}
+
+	@Override
+	public void setVorname(String vorname) {
+		// TODO Auto-generated method stub
+		super.setVorname(vorname);
+	}
+
+	@Override
+	public String getNachname() {
+		// TODO Auto-generated method stub
+		return super.getNachname();
+	}
+
+	@Override
+	public void setNachname(String nachname) {
+		// TODO Auto-generated method stub
+		super.setNachname(nachname);
+	}
+
+	public static int getInterneID() {
+		return interneID;
+	}
+
+	public static void setInterneID(int interneID) {
+		M_Trainer.interneID = interneID;
+	}
+
+	public int getTrainerID() {
+		return trainerID;
+	}
+
+	public void setTrainerID(int trainerID) {
+		this.trainerID = trainerID;
+	}
+
+	public ArrayList<M_Produkt> getProdukte() {
+		return produkte;
+	}
+
+	public void setProdukte(ArrayList<M_Produkt> produkte) {
+		this.produkte = produkte;
+	}
+	/**Ordnet einem Trainer ein weiteres Training hinzu
+	 * 
+	 * @param training
+	 */
+	public void trainingHinzufuegen(M_Training training ) {
+		if(trainings == null) {
+			trainings = new ArrayList<>();
+		}
+		trainings.add(training);
+	}
+	
+
+}
---
+++ PIEnTra/src/testdaten/Testfaelle.java
@@ -0,0 +1,16 @@
+package testdaten;
+
+import model.M_Produkt;
+
+public class Testfaelle{
+	private M_Produkt gww;
+	private M_Produkt eww;
+	private M_Produkt wwp;
+	private M_Ort or1
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
