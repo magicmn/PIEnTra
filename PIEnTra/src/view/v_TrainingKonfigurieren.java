58f65b8fb484ce2e39dd76475024ee8044269565
magicmn <julianklein1@gmail.com>
2018-03-13 11:53:49 +0100

    New view

--- PIEnTra/src/view/v_TrainingKonfigurieren.java
+++ PIEnTra/src/view/v_TrainingKonfigurieren.java
@@ -3,42 +3,104 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class v_TrainingKonfigurieren extends JFrame {
 	
 		private JTextField txt_kundenId;
 		private JTextField txt_firmenname;
 		private JTextField txt_ansprechpartner;
+		private JTextField txt_produktbeschreibung;
+		private JTextField txt_anfangsdatum;
+		private JTextField txt_enddatum;
+		private JTextField txt_tage;
+		private JTextField txt_trainer;
+		private JTextField txt_ort;
+		private JTextField txt_bemerkungen;
+		private JButton btn_kundewaehlen;
+		private JButton btn_ressourcenwaehlen;
+		private JButton btn_trainingspeichern;
+		private JButton btn_zurueck;
+		private JTextField txt_navigation;
 		
-		public static void main(String[]args) {
+		public static void main(String[]args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 			v_TrainingKonfigurieren ad = new v_TrainingKonfigurieren();
 		}
-		
-	/**  **/
+
 	public v_TrainingKonfigurieren() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("PIEnTra p1.00");
-		this.setBounds(200, 200, 800, 250);
 		initView();
 		this.setVisible(true);
 	}
 
 	private void initView() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("PIEnTra p1.00");
+		this.setBounds(200, 200, 900, 450);
+		this.setLayout(new BorderLayout());
+		//this.setResizable(false);
+		
 		JPanel pnl_center = new JPanel();
-		pnl_center.setLayout(new GridLayout(10,2));
-		JLabel lbl_kundenId = new JLabel("Kunden-ID");
-		txt_kundenId = new JTextField(10);
-		JLabel lbl_firmenname = new JLabel("Firmenname");
-		txt_firmenname = new JTextField();
-		JLabel lbl_ansprechpartner = new JLabel("Ansprechpartner");
-		txt_ansprechpartner = new JTextField(10);
-		pnl_center.add(lbl_kundenId);
-		pnl_center.add(txt_kundenId);
-		pnl_center.add(lbl_ansprechpartner);
+		pnl_center.setLayout(new GridLayout(10,2,0,10));
 		this.add(BorderLayout.CENTER, pnl_center);
+		
+		txt_kundenId = new JTextField();
+		txt_firmenname = new JTextField();
+		txt_ansprechpartner = new JTextField();
+		txt_produktbeschreibung = new JTextField();
+		txt_anfangsdatum = new JTextField();
+		txt_enddatum = new JTextField();
+		txt_tage = new JTextField();
+		txt_trainer = new JTextField();
+		txt_ort = new JTextField();
+		txt_bemerkungen = new JTextField();
+		pnl_center.add(new JLabel("Kunden-ID: "));
+		pnl_center.add(txt_kundenId);
+		pnl_center.add(new JLabel("Firmenname: "));
+		pnl_center.add(txt_firmenname);
+		pnl_center.add(new JLabel("Ansprechpartner: "));
+		pnl_center.add(txt_ansprechpartner);
+		pnl_center.add(new JLabel("Produktbeschreibung: "));
+		pnl_center.add(txt_produktbeschreibung);
+		pnl_center.add(new JLabel("Anfangsdatum: "));
+		pnl_center.add(txt_anfangsdatum);
+		pnl_center.add(new JLabel("Enddatum: "));
+		pnl_center.add(txt_enddatum);
+		pnl_center.add(new JLabel("Tage: "));
+		pnl_center.add(txt_tage);
+		pnl_center.add(new JLabel("Trainer: "));
+		pnl_center.add(txt_trainer);
+		pnl_center.add(new JLabel("Ort: "));
+		pnl_center.add(txt_ort);
+		pnl_center.add(new JLabel("Bemerkungen: "));
+		pnl_center.add(txt_bemerkungen);
+		
+		JPanel pnl_south = new JPanel();
+		pnl_south.setLayout(new GridLayout(2,1));
+		this.add(BorderLayout.SOUTH, pnl_south);
+		
+		JPanel pnl_south_top = new JPanel();
+		pnl_south.add(pnl_south_top);
+		
+		btn_kundewaehlen = new JButton("Kunde wählen");
+		btn_ressourcenwaehlen = new JButton("Ressourcen wählen");
+		btn_trainingspeichern = new JButton("Trainingspeichern");
+		btn_zurueck = new JButton("Zurück zum Hauptmenü");
+		pnl_south_top.add(btn_kundewaehlen);
+		pnl_south_top.add(btn_ressourcenwaehlen);
+		pnl_south_top.add(btn_trainingspeichern);
+		pnl_south_top.add(btn_zurueck);
+		
+		JPanel pnl_south_bottom = new JPanel();
+		pnl_south.add(pnl_south_bottom);
+		
+		txt_navigation = new JTextField();
+		pnl_south_bottom.add(txt_navigation);
 	}
 }
