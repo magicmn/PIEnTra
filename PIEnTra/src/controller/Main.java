ecef7bd47a8ec56deb26f34bbbf03a9d524c8c5f
adrian-666 <adrianfromm666@gmail.com>
2018-03-14 13:35:53 +0100

    Angepasst!

--- PIEnTra/src/controller/Main.java
+++ PIEnTra/src/controller/Main.java
@@ -1,11 +1,12 @@
 package controller;
 
-import view.V_TrainingLoeschen;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 	}
 
 }
