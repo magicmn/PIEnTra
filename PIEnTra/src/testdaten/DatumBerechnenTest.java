c7f2585d3754d51c81315d2ef52dd7f98fd97fb3
starkovic <flayzeee@gmail.com>
2018-03-15 16:08:31 +0100

    +

--- PIEnTra/src/testdaten/DatumBerechnenTest.java
+++ PIEnTra/src/testdaten/DatumBerechnenTest.java
@@ -5,14 +5,10 @@ import utils.SimpleDatumBerechnen;
 public class DatumBerechnenTest {
 	
 	public static void main(String[] args) {
-		String a = "11.11.2012";
-		String b = "21.11.2012";
+		String a = "01.01.2016";
+		String b = "01.01.2017";
 		
 		System.out.println(SimpleDatumBerechnen.datumBerechnen(a, b));
-		System.out.println(SimpleDatumBerechnen.datumBerechnen(a, 5));
+		System.out.println(SimpleDatumBerechnen.datumBerechnen(a, 22));
 	}
-	
-
-	
-
 }
