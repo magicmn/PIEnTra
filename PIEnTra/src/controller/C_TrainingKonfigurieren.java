b908944e1ce253a1375a1d0ca27c949ea018a9b8
Konstantin <Konstantin@DESKTOP-FURLC90>
2018-03-15 15:44:10 +0100

    push

--- PIEnTra/src/controller/C_TrainingKonfigurieren.java
+++ PIEnTra/src/controller/C_TrainingKonfigurieren.java
@@ -23,32 +23,7 @@ public class C_TrainingKonfigurieren {
 	
 	public C_TrainingKonfigurieren(){
 		view = new V_TrainingKonfigurieren();
-		initListener();
+		
 	}	
-	private void initListener() {
-		view.addBtn_kundeWaehlenListener(new KundeWaehlen());
-		view.addBtn_ressourceWaehlenListener(new RessourceWaehlen());
-		view.addBtn_trainingSpeichernListener(new TrainingSpeichern());
-		view.addBtn_zurueckListener(new Zurueck());
-	}
-	private class KundeWaehlen implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("Kunde waehlen!");		
-		}
-	}
-	private class RessourceWaehlen implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("Ressource Wählen");		
-		}
-	}
-	private class TrainingSpeichern implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("Training Speichern!");
-		}
-	}
-	private class Zurueck implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			SimpleSwitchFrame.switchFrame(view, new C_Hauptmenue());		
-		}
-	}
+	
 }
