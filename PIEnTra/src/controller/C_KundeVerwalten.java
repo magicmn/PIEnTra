ce5759b9998188308480cad120af3e86d4721766
Konstantin <Konstantin@DESKTOP-FURLC90>
2018-03-15 16:34:09 +0100

    singelton

--- PIEnTra/src/controller/C_KundeVerwalten.java
+++ PIEnTra/src/controller/C_KundeVerwalten.java
@@ -1,6 +1,6 @@
 /**
- * Aktuelle Version: 1.2 
- * Authoren: Julian (1.0), Adrian (1.1, 1.2), Andreas (1.1)
+ * Aktuelle Version: 1.3
+ * Authoren: Julian (1.0), Adrian (1.1, 1.2), Andreas (1.1), K0nstantin (1.3)
  * 
  * Changelog:
  * 1.0 	
@@ -9,6 +9,8 @@
  * 		-ActionListener hinzugefügt!
  * 1.2
  * 		-ActionListener wieder entfernt und in View implementiert!
+ * 1.3
+ * 		-Singelton Implementiert
  **/
 
 
@@ -18,12 +20,12 @@ import model.M_Kunde;
 import view.V_KundeVerwalten;
 
 public class C_KundeVerwalten {
-
+	private static C_KundeVerwalten instance = null;
 	private static V_KundeVerwalten view;
-	
+
 	private M_Kunde kunde;
-	
-	public C_KundeVerwalten(){
+
+	private C_KundeVerwalten(){
 		view = new V_KundeVerwalten();
 	}
 
@@ -33,12 +35,21 @@ public class C_KundeVerwalten {
 	public void setView(V_KundeVerwalten view) {
 		this.view = view;
 	}
-	
+
 	public M_Kunde getKunde() {
 		return kunde;
 	}
 	public void setKunde(M_Kunde kunde) {
 		this.kunde = kunde;
 	}
+
+	public static C_KundeVerwalten getInstance() {
+
+		if(instance==null) {
+			instance = new C_KundeVerwalten();
+		}
+		return instance;
+	}
 }
+
 
