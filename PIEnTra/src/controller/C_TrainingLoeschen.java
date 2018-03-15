f4ea592bafc241021a5a7ebcba02a09a07849b9f
Teilnehmer <Teilnehmer@CSC-Akademie_02>
2018-03-15 15:16:26 +0100

    ActionListener in View hinzugefügt
    Controller angepasst

--- PIEnTra/src/controller/C_TrainingLoeschen.java
+++ PIEnTra/src/controller/C_TrainingLoeschen.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class C_TrainingLoeschen {
+import view.V_TrainingLoeschen;
 
+public class C_TrainingLoeschen {
+	private static V_TrainingLoeschen view;
+	
+	public C_TrainingLoeschen(){
+		view = new V_TrainingLoeschen();
+	}
+	public static V_TrainingLoeschen getView() {
+		return view;
+	}
+	public void setView(V_TrainingLoeschen view) {
+		this.view = view;
+	}
+	
 }
