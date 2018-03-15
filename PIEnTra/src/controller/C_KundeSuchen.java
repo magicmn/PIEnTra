65e4ed92c6819a2db943f678aca371cb5947b064
adrian-666 <adrianfromm666@gmail.com>
2018-03-15 11:05:50 +0100

    Angepasst!

--- PIEnTra/src/controller/C_KundeSuchen.java
+++ PIEnTra/src/controller/C_KundeSuchen.java
@@ -32,11 +32,13 @@ public class C_KundeSuchen {
 	private class KundeSuchen implements ActionListener {
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Kunde suchen!");
+			
 		}
 	}
 	private class Abbrechen implements ActionListener {
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Abbrechen");
+			view.setVisible(false);
 		}
 	}
 }
