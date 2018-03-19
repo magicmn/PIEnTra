1f84f9da82b6efbcac627bdc57d533714b616276
magicmn <julianklein1@gmail.com>
2018-03-19 09:25:48 +0100

    sad

--- PIEnTra/src/view/V_KundeVerwalten.java
+++ PIEnTra/src/view/V_KundeVerwalten.java
@@ -1,223 +1,234 @@
-/**
- * Aktuelle Version: 1.3 
- * Authoren: Adrian 
- * 
- * Changelog:
- * 
- * 1.3
- * 		-SimpleSwitchFrame implementiert!
- * 1.2
- * 		-ActionListener hinzugefügt!
- * 1.1
- * 		-Get / Set hinzugefügt!
- * 1.0 	
- * 		-View erstellt!
- * 
- **/
-
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.MaskFormatter;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-import model.M_Kunde;
 import controller.C_Hauptmenue;
 import controller.C_KundeSuchen;
 import controller.C_KundeVerwalten;
-import utils.SimpleFormatter;
+import utils.SimpleMasterWindow;
 import utils.SimpleSwitchFrame;
 import utils.SimpleTextPanel;
 
-public class V_KundeVerwalten extends JFrame {
+/**
+ * View von TrainingKonfigurieren.
+ * @version 1.4 Erbt nun von Superklasse {@link SimpleMasterWindow}.
+ * @version 1.3 SimpleSwitchFrame implementiert.
+ * @version 1.2 Listener hinzugefügt.
+ * @version 1.1 Listener entfernt. Getter und Setter entfernt.
+ * @version 1.0 View implementiert. 
+ * @author Adrian Fromm
+ * @author Julian Klein
+ * @author Konstantin Frei
+ * @see {@link controller.C_Hauptmenue};
+ */
+public class V_KundeVerwalten extends SimpleMasterWindow {
+	private static final long serialVersionUID = -6381551589496678636L;
 	
-	private JPanel pnl_center;
+	/* Deklaration und Initailiserung von verschiedenen Variablen **/
+	
+	/** 
+	 * Standard Größe des Fensters.
+	 * @see SimpleMasterWindow#initFrame(Dimension defaultSize, Dimension minSize)
+	 * */
+	public static Dimension defaultSize = new Dimension(850, 450);
+	/** 
+	 * Minimale Größe des Fensters.
+	 * @see SimpleMasterWindow#initFrame(Dimension defaultSize, Dimension minSize)
+	 * */
+	private static Dimension minSize = new Dimension(700, 200);
+	/**
+	 * Text der in der Naviagtionsleiste ausgegebn wird.
+	 * @see SimpleMasterWindow#initSouth(String navigationText)
+	 */
+	private static String navigationText = "Training konfigurieren / Kunde wählen";
+	
 	private SimpleTextPanel pnl_kundenID = new SimpleTextPanel("Kunden-ID:");
 	private SimpleTextPanel pnl_firmenname = new SimpleTextPanel("Firmenname:");
 	private SimpleTextPanel pnl_ansprechpartner = new SimpleTextPanel("Ansprechpartner:");
 	private SimpleTextPanel pnl_branche = new SimpleTextPanel("Branche:");
-	private SimpleTextPanel pnl_email = new SimpleTextPanel("Email:");
+	private SimpleTextPanel pnl_email = new SimpleTextPanel("E-Mail:");
 	private SimpleTextPanel pnl_rufnummer = new SimpleTextPanel("Rufnummer:");
-	private SimpleTextPanel pnl_addresse = new SimpleTextPanel("Addresse");
-	private SimpleTextPanel pnl_strasse = new SimpleTextPanel("Straße:");
-	private SimpleTextPanel pnl_plz = new SimpleTextPanel("Postleitzahl:", SimpleFormatter.getPLZFormat());
+	private JLabel lbl_adresse = new JLabel("Adresse");
+	private SimpleTextPanel pnl_strasse = new SimpleTextPanel("Strasse:");
+	private SimpleTextPanel pnl_postleitzahl = new SimpleTextPanel("Postleitzahl:");
 	private SimpleTextPanel pnl_bundesland = new SimpleTextPanel("Bundesland:");
 	private SimpleTextPanel pnl_stadt = new SimpleTextPanel("Stadt:");
 	
-	private JPanel pnl_south;
-	private JButton btn_kundesuchen = new JButton("Kunde suchen");
-	private JButton btn_kundeaktualisieren = new JButton("Kunde aktualisieren");
-	private JButton btn_kundeanlegen = new JButton("Neuen Kunden anlegen");
-	private JButton btn_zurueck = new JButton("Zurück zum Hauptmenü");
-	private JTextField txt_navigation;
+	private JButton btn_kundeSuchen = new JButton("Kunde suchen");
+	private JButton btn_kundeAktualisieren = new JButton("Kunde wählen");
+	private JButton btn_kundeAnlegen = new JButton("Kunde suchen");
+	private JButton btn_zurueck = new JButton("Zurück zu Training konfigurieren");
 	
-	private C_KundeSuchen suchview;
-	public V_KundeVerwalten thisView;
+	private V_KundeVerwalten thisView;
 	
+	/* Konstruktor und Methoden die vom Konstruktor aufgerufen werden. */
+	
+	/**
+	 * Konstruktor der View Hauptmenue.
+	 * Übergibt an die Superklasse die standard und minimal Größe, sowie aktuelle Pfadangaben der Navigationsleiste.
+	 * Initialisiere dann den Content und lösche ein überflüssiges Element aus der im Hauptmenu nicht benutzten Menuleiste.
+	 * Zuletzt werden die Listener initialisiert.
+	 */
 	public V_KundeVerwalten() {
+		super(
+			defaultSize,
+			minSize,
+			navigationText
+		);
 		this.thisView = this;
-		initView();
-		resizeGUI();
+		initContent();
+		initMenu();
 		initListener();
-		this.setVisible(true);
+		resizeGUI();
 	}
 
-	private void initView() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("PIEnTra p1.00");
-		this.setSize(750, 490); // Optimale Größe die beim Starten geladen wird.
-		this.setMinimumSize(new Dimension(500, 400)); // Um zu verhindern, dass der DAU sich wundert warum das Fenster auf einmal "weg" ist.
-		this.setLayout(new BorderLayout());
-		this.setLocationRelativeTo(null); // Zentriert Frame in der Mitte des Bildschirms.
-		this.addComponentListener(new ResizeListener());  // Fügt Listener für Frame veränderungen hinzu.
-		
-		pnl_addresse.getTxtField().setVisible(false);
-		pnl_addresse.setBorder(new EmptyBorder(8,0,0,0));
-		
-		pnl_center = new JPanel(new GridLayout(11, 1, 2 ,2));
-		pnl_center.add(pnl_kundenID);
-		pnl_center.add(pnl_firmenname);
-		pnl_center.add(pnl_ansprechpartner);
-		pnl_center.add(pnl_branche);
-		pnl_center.add(pnl_email);
-		pnl_center.add(pnl_rufnummer);
-		pnl_center.add(pnl_addresse);
-		pnl_center.add(pnl_strasse);
-		pnl_center.add(pnl_plz);
-		pnl_center.add(pnl_bundesland);
-		pnl_center.add(pnl_stadt);
-		this.add(BorderLayout.CENTER, pnl_center);
-		pnl_kundenID.getTxtField().setEditable(false);
-	
-	
-		
-		pnl_south = new JPanel(new GridLayout(2, 1));
-		JPanel pnl_south_top = new JPanel(new GridLayout(1, 4));
-		pnl_south_top.setBorder(new EmptyBorder(10,8,5,10));
-		pnl_south_top.add(btn_kundesuchen);
-		pnl_south_top.add(btn_kundeaktualisieren);
-		pnl_south_top.add(btn_kundeanlegen);
-		pnl_south_top.add(btn_zurueck);
-		
-		pnl_south.add(pnl_south_top);
-		JPanel pnl_south_bottom = new JPanel(new FlowLayout());
-		pnl_south_bottom.add(txt_navigation = new JTextField("PIEnTra / Kunde verwalten"));
-		txt_navigation.setFocusable(false);
-		pnl_south.add(pnl_south_bottom);
-		this.add(BorderLayout.SOUTH, pnl_south);
+	/**
+	 * Initialisiere den Inhalt des Centers.
+	 */
+	private void initContent() {
+		JPanel pnl_content = new JPanel();
+		pnl_content.setLayout(new GridLayout(11, 1));
+		pnl_content.setAlignmentY(LEFT_ALIGNMENT);
+		pnl_content.add(pnl_kundenID);
+		pnl_content.add(pnl_firmenname);
+		pnl_content.add(pnl_ansprechpartner);
+		pnl_content.add(pnl_branche);
+		pnl_content.add(pnl_email);
+		pnl_content.add(pnl_rufnummer);
+		pnl_content.add(lbl_adresse);
+		pnl_content.add(pnl_strasse);
+		pnl_content.add(pnl_postleitzahl);
+		pnl_content.add(pnl_bundesland);
+		pnl_content.add(pnl_stadt);
+		super.getPnl_center().add(pnl_content);
 	}
 	
-	/** Enthält variable Gößen **/
-	private void resizeGUI() {
-		txt_navigation.setPreferredSize(new Dimension(this.getWidth() - 30, txt_navigation.getPreferredSize().height));
-		pnl_kundenID.setTxtField_Size(pnl_center.getWidth() / 4);
-		pnl_firmenname.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_ansprechpartner.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_branche.setTxtField_Size(pnl_center.getWidth() / 4);
-		pnl_email.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_rufnummer.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_strasse.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_plz.setTxtField_Size(pnl_center.getWidth() / 4);
-		pnl_bundesland.setTxtField_Size(pnl_center.getWidth() / 2);
-		pnl_stadt.setTxtField_Size((pnl_center.getWidth() / 2));
+	/**
+	 * Initiailisert die Menu Buttons
+	 */
+	private void initMenu() {
+		getPnl_menu().add(btn_kundeSuchen);
+		getPnl_menu().add(btn_kundeAktualisieren);
+		getPnl_menu().add(btn_kundeAnlegen);
+		getPnl_menu().add(btn_zurueck);
+		
 	}
 	
-	private void initListener(){
-		btn_kundesuchen.addActionListener(new KundeSuchen());
-		btn_kundeaktualisieren.addActionListener(new KundeAktualisieren());
-		btn_kundeanlegen.addActionListener(new KundeAnlegen());
+	/**
+	 * Initialisiert ActionListener
+	 */
+	private void initListener() {
+		btn_kundeSuchen.addActionListener(new KundeSuchen());
+		btn_kundeAktualisieren.addActionListener(new KundeAktualisieren());
+		btn_kundeAnlegen.addActionListener(new KundeAnlegen());
 		btn_zurueck.addActionListener(new Zurueck());
 	}
 	
-	class ResizeListener implements ComponentListener {
-		public void componentResized(ComponentEvent arg0) {
-			resizeGUI();
+	protected void resizeGUI() {
+		int maxWidthTextBox = this.getWidth() - SimpleTextPanel.getLabelWidth() - (getPadding() * 2) - 25;
+		int optimalButtonWidth = (int) super.getPnl_menu().getWidth() - 25;
+		pnl_kundenID.setTextFieldWidth(maxWidthTextBox / 4);
+		pnl_firmenname.setTextFieldWidth(maxWidthTextBox / 2);
+		pnl_ansprechpartner.setTextFieldWidth(maxWidthTextBox / 2);
+		pnl_branche.setTextFieldWidth(maxWidthTextBox / 4);
+		pnl_email.setTextFieldWidth(maxWidthTextBox / 2);
+		pnl_rufnummer.setTextFieldWidth(maxWidthTextBox / 2);
+		lbl_adresse.setPreferredSize(new Dimension(SimpleTextPanel.getLabelWidth(), lbl_adresse.getPreferredSize().height));
+		pnl_strasse.setTextFieldWidth(maxWidthTextBox / 2);
+		pnl_postleitzahl.setTextFieldWidth(maxWidthTextBox / 4);
+		pnl_bundesland.setTextFieldWidth(maxWidthTextBox / 2);
+		pnl_stadt.setTextFieldWidth(maxWidthTextBox / 2);
+		btn_kundeSuchen.setPreferredSize(new Dimension(optimalButtonWidth / 4 , btn_kundeSuchen.getPreferredSize().height));
+		btn_kundeAktualisieren.setPreferredSize(new Dimension(optimalButtonWidth / 4, btn_kundeSuchen.getPreferredSize().height));
+		btn_kundeAnlegen.setPreferredSize(new Dimension(optimalButtonWidth / 4, btn_kundeSuchen.getPreferredSize().height));
+		btn_zurueck.setPreferredSize(new Dimension(optimalButtonWidth / 4, btn_kundeSuchen.getPreferredSize().height));
+		
+	}
+
+	/* Implementierung der ActionListener */
+	
+	public static void main(String [] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
 		}
-		public void componentHidden(ComponentEvent arg0) {}
-		public void componentMoved(ComponentEvent arg0) {}
-		public void componentShown(ComponentEvent arg0) {}
+		new V_KundeVerwalten();
 	}
 	
-	// Get und Set
+	// Getter und Setter
 	
 	public String getText_pnl_kundenID(){
-		return pnl_kundenID.getString();
+		return pnl_kundenID.getText();
 	}
 	public String getText_pnl_firmenname(){
-		return pnl_firmenname.getString();
+		return pnl_firmenname.getText();
 	}
 	public String getText_pnl_ansprechpartner() {
-		return pnl_ansprechpartner.getString();
+		return pnl_ansprechpartner.getText();
 	}
 	public String getText_pnl_branche() {
-		return pnl_branche.getString();
+		return pnl_branche.getText();
 	}
 	public String getText_pnl_email() {
-		return pnl_email.getString();
+		return pnl_email.getText();
 	}
 	public String getText_pnl_rufnummer() {
-		return pnl_rufnummer.getString();
+		return pnl_rufnummer.getText();
 	}
 	public String getText_pnl_strasse() {
-		return pnl_strasse.getString();
+		return pnl_strasse.getText();
 	}
 	public String getText_pnl_plz() {
-		return pnl_plz.getString();
+		return pnl_postleitzahl.getText();
 	}
 	public String getText_pnl_bundesland() {
-		return pnl_bundesland.getString();
+		return pnl_bundesland.getText();
 	}
 	public String getText_pnl_stadt() {
-		return pnl_stadt.getString();
+		return pnl_stadt.getText();
 	}
-	
-	//------------------------------------------------------
-	
 	public void setText_pnl_kundenID(String text){
-		this.pnl_kundenID.setString(text);
+		this.pnl_kundenID.setText(text);
 	}
 	public void setText_pnl_firmenname(String text){
-		this.pnl_firmenname.setString(text);
+		this.pnl_firmenname.setText(text);
 	}
 	public void setText_pnl_ansprechpartner(String text) {
-		this.pnl_ansprechpartner.setString(text);;
+		this.pnl_ansprechpartner.setText(text);;
 	}
 	public void setText_pnl_branche(String text) {
-		this.pnl_branche.setString(text);
+		this.pnl_branche.setText(text);
 	}
 	public void setText_pnl_email(String text) {
-		this.pnl_email.setString(text);
+		this.pnl_email.setText(text);
 	}
 	public void setText_pnl_rufnummer(String text) {
-		this.pnl_rufnummer.setString(text);
+		this.pnl_rufnummer.setText(text);
 	}
 	public void setText_pnl_strasse(String text) {
-		this.pnl_strasse.setString(text);
+		this.pnl_strasse.setText(text);
 	}
 	public void setText_pnl_plz(String text) {
-		this.pnl_plz.setString(text);
+		this.pnl_postleitzahl.setText(text);
 	}
 	public void setText_pnl_bundesland(String text) {
-		this.pnl_bundesland.setString(text);
+		this.pnl_bundesland.setText(text);
 	}
 	public void setText_pnl_stadt(String text) {
-		this.pnl_stadt.setString(text);
+		this.pnl_stadt.setText(text);
 	}
+	// ActionListener
 	
-	//Action Listener
-
 	private class KundeSuchen implements ActionListener {
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Kunde suchen!");
@@ -241,109 +252,4 @@ public class V_KundeVerwalten extends JFrame {
 			SimpleSwitchFrame.switchFrame(thisView, C_Hauptmenue.getInstance(), C_Hauptmenue.getInstance().getView());
 		}
 	}
-
-	public JPanel getPnl_center() {
-		return pnl_center;
-	}
-
-	public void setPnl_center(JPanel pnl_center) {
-		this.pnl_center = pnl_center;
-	}
-
-	public SimpleTextPanel getPnl_kundenID() {
-		return pnl_kundenID;
-	}
-
-	public void setPnl_kundenID(SimpleTextPanel pnl_kundenID) {
-		this.pnl_kundenID = pnl_kundenID;
-	}
-
-	public SimpleTextPanel getPnl_firmenname() {
-		return pnl_firmenname;
-	}
-
-	public void setPnl_firmenname(SimpleTextPanel pnl_firmenname) {
-		this.pnl_firmenname = pnl_firmenname;
-	}
-
-	public SimpleTextPanel getPnl_ansprechpartner() {
-		return pnl_ansprechpartner;
-	}
-
-	public void setPnl_ansprechpartner(SimpleTextPanel pnl_ansprechpartner) {
-		this.pnl_ansprechpartner = pnl_ansprechpartner;
-	}
-
-	public SimpleTextPanel getPnl_branche() {
-		return pnl_branche;
-	}
-
-	public void setPnl_branche(SimpleTextPanel pnl_branche) {
-		this.pnl_branche = pnl_branche;
-	}
-
-	public SimpleTextPanel getPnl_email() {
-		return pnl_email;
-	}
-
-	public void setPnl_email(SimpleTextPanel pnl_email) {
-		this.pnl_email = pnl_email;
-	}
-
-	public SimpleTextPanel getPnl_rufnummer() {
-		return pnl_rufnummer;
-	}
-
-	public void setPnl_rufnummer(SimpleTextPanel pnl_rufnummer) {
-		this.pnl_rufnummer = pnl_rufnummer;
-	}
-
-	public SimpleTextPanel getPnl_addresse() {
-		return pnl_addresse;
-	}
-
-	public void setPnl_addresse(SimpleTextPanel pnl_addresse) {
-		this.pnl_addresse = pnl_addresse;
-	}
-
-	public SimpleTextPanel getPnl_strasse() {
-		return pnl_strasse;
-	}
-
-	public void setPnl_strasse(SimpleTextPanel pnl_strasse) {
-		this.pnl_strasse = pnl_strasse;
-	}
-
-	public SimpleTextPanel getPnl_plz() {
-		return pnl_plz;
-	}
-
-	public void setPnl_plz(SimpleTextPanel pnl_plz) {
-		this.pnl_plz = pnl_plz;
-	}
-
-	public SimpleTextPanel getPnl_bundesland() {
-		return pnl_bundesland;
-	}
-
-	public void setPnl_bundesland(SimpleTextPanel pnl_bundesland) {
-		this.pnl_bundesland = pnl_bundesland;
-	}
-
-	public SimpleTextPanel getPnl_stadt() {
-		return pnl_stadt;
-	}
-
-	public void setPnl_stadt(SimpleTextPanel pnl_stadt) {
-		this.pnl_stadt = pnl_stadt;
-	}
-
-	public JPanel getPnl_south() {
-		return pnl_south;
-	}
-
-	public void setPnl_south(JPanel pnl_south) {
-		this.pnl_south = pnl_south;
-	}
-
 }
