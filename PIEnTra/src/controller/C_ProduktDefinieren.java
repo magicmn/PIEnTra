0c172bb9e80e3173ce5d7dee56039f9127204de9
Teilnehmer <Teilnehmer@CSC-Akademie_02>
2018-03-20 13:00:09 +0100

    Objekte können nun erstellt werden

--- PIEnTra/src/controller/C_ProduktDefinieren.java
+++ PIEnTra/src/controller/C_ProduktDefinieren.java
@@ -1,28 +1,28 @@
 /**
- * Aktuelle Version: 1.2  
- * Authoren: Adrian (1.0, 1.1, 1.2), Konstantin (1.3)
- * 
- * Changelog:
- * 1.3
- * 		-Singelton implementiert
- * 1.2
- * 		-ActionListener wieder entfernt und in View implementiert!
- * 1.1
- * 		-ActionListener hinzugefügt!
- * 1.0 	
- * 		-Controller erstellt
+ * View von TrainingKonfigurieren.
+ * @version 1.4 produktDefinieren Funktion hinzugefügt
+ * @version 1.3 Singelton implementiert
+ * @version 1.2 ActionListener wieder entfernt und in View implementiert!
+ * @version 1.1 ActionListener hinzugefügt!
+ * @version 1.0 Controller erstellt
+ * @author Adrian Fromm
+ * @author Andreas Kann
+ * @author Julian Klein
  **/
 
 package controller;
 
 import javax.swing.JFrame;
 
+import model.M_Produkt;
+import utils.SimpleMasterWindow;
 import view.V_ProduktDefinieren;
 
 public class C_ProduktDefinieren {
 	
 	private static C_ProduktDefinieren instance = null;
 	private static V_ProduktDefinieren view;
+	private M_Produkt produkt;
 
 	private C_ProduktDefinieren(){
 		view = new V_ProduktDefinieren();
@@ -37,6 +37,12 @@ public class C_ProduktDefinieren {
 			instance = new C_ProduktDefinieren();
 		}
 		return instance;
+	}
+	
+	public void produktDefinieren(){
+		
+		produkt = new M_Produkt(view.getText_txt_produktID(), view.getText_txt_produktbezeichnung(), view.getText_txt_produktbeschreibung(), 1.0);
+		
 	}
 	
 	
