a3390423d20a3634be7d699fd3a350826c6a2cbe
julia <julia@DESKTOP-PE81K2H>
2018-03-21 13:39:45 +0100

    qwe

--- PIEnTra/src/model/package-info.java
+++ PIEnTra/src/model/package-info.java
@@ -1,10 +1,7 @@
 /** In diesem Package sind alle ModelKlassen enthalten.
  * Diese wurden aus der Aufgabenstellung entnommen und nach bedarf erweitert(für nähere information sieh 
- * @see <a href="file:///PIEnTra/src/model/ModelKlassendiagramm.ucls">ModelLassendiagramm</a>
+ * @see <a href="file:///PIEnTra/diagramms/modelKlassendia.ucls">Modelklassendiagramm</a>
  * @file ModelKlassendiagramm.ucls
- */
-/**
- * @author Konstantin
- *
+ * @author Konstantin Frei
  */
 package model;
